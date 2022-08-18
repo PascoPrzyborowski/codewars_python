3775c6915bd930f871c3b283c163a49d99f204ee
PascoPrzyborowski <unity01@gmx.net>
2022-08-18 09:50:04 +0200

    cw_ex1

---
+++ cw_ex1.py
@@ -0,0 +1,7 @@
+
+
+ausgabetext = "Der Preis für 2 Socken beträgt 5 DM und 5 Paar kosten 10 DM"
+print(ausgabetext)
+ausgabetext = ausgabetext.replace("DM", "Euro")
+print("Nach dem Austauschen über replace():")
+print(ausgabetext)
